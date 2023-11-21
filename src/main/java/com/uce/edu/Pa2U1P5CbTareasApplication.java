8c7c0eef4773485563ad8bf36bc2cbaa5cbbf23c
ChrizBeta <chrisalejo@hotmail.es>
2023-11-20 22:42:50 -0500

    punto2

--- src/main/java/com/uce/edu/Pa2U1P5CbTareasApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5CbTareasApplication.java
@@ -1,13 +1,21 @@
 package com.uce.edu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pa2U1P5CbTareasApplication {
+public class Pa2U1P5CbTareasApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5CbTareasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("PUNTO 2: CAMBIO Y MISMA RAMA");
+		
 	}
 
 }
